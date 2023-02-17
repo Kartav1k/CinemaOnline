2f8050365b9f0465fbc1fab400901d899c2dcc77
Kartav1k <anton.savitskiy.02@mail.ru>
2023-02-18 01:08:43 +0300

    Создан метод onCreate()

--- app/src/main/java/com/example/cinemaonline/MainActivity.java
+++ app/src/main/java/com/example/cinemaonline/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cinemaonline;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context info = getApplicationContext();
+        CharSequence message = "Created";
+        int duration = Toast.LENGTH_SHORT   ;
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+        Log.e(TAG, "Fail");
+        Log.w(TAG, "Warning");
+        Log.i(TAG, "Information");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Detail");
     }
 }
