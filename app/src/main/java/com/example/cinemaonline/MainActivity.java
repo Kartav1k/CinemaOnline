3ed0fecdb50463078ae85fa843ced66a6757e524
Anton <anton.savitskiy.02@mail.ru>
2023-03-10 00:32:01 +0300

    Наконец-то реализовано появление сообщения при нажатии на кнопку программным способом

--- app/src/main/java/com/example/cinemaonline/MainActivity.java
+++ app/src/main/java/com/example/cinemaonline/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,16 +14,18 @@ import android.util.Log;
 import android.widget.Toast;
 import android.os.Bundle;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CinemaOnline";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView=(TextView) findViewById(R.id.textView);
+        TextView textView=findViewById(R.id.textView);
         textView.setText("Авторизация");
+        TextView butOut=(TextView)findViewById(R.id.butOut);
         ImageView imageView=findViewById(R.id.imageLogo);
         imageView.setImageResource(R.drawable.logo1);
         Button button=findViewById(R.id.button);
@@ -36,12 +39,17 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT   ;
         Toast toast = Toast.makeText(info, message, duration);
         toast.show();
-    }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                butOut.setText("Вы авторизированы!");
+            }
+        });
 
+    }
     @Override
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Start activity";
         int duration = Toast.LENGTH_SHORT;
@@ -52,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Resume activity";
         int duration = Toast.LENGTH_SHORT;
@@ -63,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause(){
         super.onPause();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Pause activity";
         int duration = Toast.LENGTH_SHORT;
@@ -74,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop(){
         super.onStop();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Stop activity";
         int duration = Toast.LENGTH_SHORT;
@@ -85,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart(){
         super.onRestart();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Restart activity";
         int duration = Toast.LENGTH_SHORT;
@@ -96,12 +100,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        setContentView(R.layout.activity_main);
         Context info = getApplicationContext();
         CharSequence message = "Destroy activity";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(info, message, duration);
         toast.show();
     }
+
 
 }
